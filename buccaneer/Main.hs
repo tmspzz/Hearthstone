a03c63a208121cf6b38ec5e9f8fae6eaca2264bd
Tommaso Piazza <tommaso.piazza@gmail.com>
2016-01-11 15:21:28 +0100

    Adopting Options.Applicative

--- buccaneer/Main.hs
+++ buccaneer/Main.hs
@@ -4,25 +4,71 @@ module Main where
   import qualified System.Directory as D
   import qualified Deckhand.Harpoons.Heartpwn as HPWN
   import qualified Deckhand.Deck as DK
-  import Data.List (intersperse)
+  import qualified Network.URI as N
+  import Data.List (intersperse, nub)
   import Control.Concurrent.Async
   import Data.Functor
   import System.Environment
+  import Options.Applicative
+  import Control.Monad
+  import Control.Monad.Trans
+  import Control.Monad.Trans.Maybe
 
 
+  parseCommand :: Parser Command
+  parseCommand = subparser $
+          command "rip"    (parseRip `withInfo` "Rip one of more deck URLs") <>
+          command "scrape" (parseScrape `withInfo` ("Scrape a Heartpwn source for all decks. One or more of " ++ sources))
+          where
+            sources = unwords (intersperse "|" (map show [HPWN.Hot, HPWN.New, HPWN.Week, HPWN.Month, HPWN.AllTime]))
+
+  withInfo :: Parser a -> String -> ParserInfo a
+  withInfo opts desc = info (helper <*> opts) $ progDesc desc
+
+  parseRip :: Parser Command
+  parseRip = Rip <$> some (argument str (metavar "[URL ...]"))
+
+  parseScrape :: Parser Command
+  parseScrape = Scrape <$> some (argument str (metavar "[SOURCE ...]"))
 
   type UrlAndFileName = (String, String)
 
+  data Command
+    = Rip [String]
+    | Scrape [String]
+
   main :: IO ()
   main = do
-    args <- getArgs
-    case args of
-      [] -> putStrLn $ "You need to specify one or more of " ++ unwords (intersperse "|" (map show [HPWN.Hot, HPWN.New, HPWN.Week, HPWN.Month, HPWN.AllTime]))
-      as  -> do
-        _ <- mapConcurrently getDecksFromHeartpwnSource (map read as)
-        return ()
+    let opts = info (helper <*> parseCommand) (header "Rips decks for cards" )
+    cmd <- execParser opts
+    case cmd of
+      Rip urls -> void $ mapConcurrently (runMaybeT . getDeckFromURL) (nub urls)
+      Scrape sources -> void $ mapConcurrently getDecksFromHeartpwnSource (map (read . nub) sources)
 
 
+  getDeckFromURL :: String -> MaybeT IO ()
+  getDeckFromURL urlString = MaybeT (pure . N.parseURI$ urlString)
+    >>= getDeckFromURI
+
+  getDeckFromURI :: N.URI -> MaybeT IO ()
+  getDeckFromURI uri = MaybeT ( pure . N.uriAuthority $ uri)
+    >>= pure . N.uriRegName
+    >>= \x ->  case x of
+      "www.hearthpwn.com" -> lift . getDeckFromHeartpwnURI $ uri
+      "www.tempostorm.com" -> lift . getDeckFromTempostormURI $ uri
+      otherwise -> lift $ void (putStrLn ("Unsupported URL " ++ N.uriToString id uri ""))
+
+  getDeckFromHeartpwnURI :: N.URI -> IO ()
+  getDeckFromHeartpwnURI uri = do
+    filesInCache <- getFilesFromCache
+    saveDeckToFile urlAndFileName
+    where
+      fileName = HPWN.fileNameFromDeckURI $ N.uriPath uri
+      url = N.uriToString id uri ""
+      urlAndFileName = (url, fileName)
+
+  getDeckFromTempostormURI :: N.URI -> IO ()
+  getDeckFromTempostormURI uri = undefined
 
   getDecksFromHeartpwnSource :: HPWN.HeartpwnDeckSource -> IO ()
   getDecksFromHeartpwnSource source = do
@@ -36,8 +82,6 @@ module Main where
 
   saveDeckToFile :: UrlAndFileName -> IO ()
   saveDeckToFile (url, fileName) = putStrLn ("*** Getting deck at " ++ url) >> HPWN.getDeck url >>= (\d -> DK.toFile d (deckhandDeckCache `FP.combine` fileName))
-
-
 
   getFilesFromCache :: IO [FilePath]
   getFilesFromCache = do
